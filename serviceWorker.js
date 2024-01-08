1fb7ce62df3be0ec7f2c081160231bc72d0c1e72
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-01-09 00:42:59 +0530

    serviceWorker added for offline usage

---
+++ serviceWorker.js
@@ -0,0 +1,23 @@
+const staticTimeTable = "time-table-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/timetable.png",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticTimeTable).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
