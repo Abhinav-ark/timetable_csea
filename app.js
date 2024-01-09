b9b9890af02ef69cbab436941dcdf3c2c3fd8788
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-01-09 15:09:04 +0530

    serviceWorker bug fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
-        .register("/serviceWorker.js", { scope: '/' })
+        .register("/serviceWorker.js")
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     })
