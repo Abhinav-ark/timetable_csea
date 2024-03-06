a1cd7dc5acfc26c916b27f0f932746506e48a578
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-03-06 16:11:54 +0530

    added support for safari
    Co-authored-by: Ashwin <ashrockzzz2003@gmail.com>

---
+++ tasks.js
@@ -0,0 +1,16 @@
+const tasks = 
+[
+    {
+        "due":"22-02-2024",
+        "task":"19CSE314 (SE) Chart Work"
+    },   
+    {
+        "due":"06-03-2024",
+        "task":"19CSE312 (DS) Tutorial"
+    },
+
+    {
+        "due":"Not Known",
+        "task":"Mock Interview Starts R.No 1-25"
+    }
+]
