aa6eed157ad3fd98d55cca02a666918cbd44956c
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-03-07 23:31:49 +0530

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -1,16 +1,33 @@
 const tasks = 
 [
     {
-        "due":"22-02-2024",
-        "task":"19CSE314 (SE) Chart Work"
-    },   
+        "due":"March 10th",
+        "task":"POPL Assignments(x2)"
+    },
+    
+
     {
-        "due":"06-03-2024",
-        "task":"19CSE312 (DS) Tutorial"
+        "due":"March 11th",
+        "task":"POPL Quiz-1"
+    },
+    
+    {
+        "due":"March 11th to 15th",
+        "task":"DS CS Review-1"
     },
 
     {
-        "due":"Not Known",
-        "task":"Mock Interview Starts R.No 1-25"
+        "due":"March 12th",
+        "task":"POPL Lab Eval-1"
+    },
+
+    {
+        "due":"March 13th",
+        "task":"CS Tutorial-1"
+    },
+
+    {
+        "due":"Starting April 1st",
+        "task":"CIR Verbal Speech"
     }
 ]
