90291ea93259ed8ef8b70fd73bd906681b7eb56c
Hariharan18 <93380054+Hariharan-Arul@users.noreply.github.com>
2024-03-13 00:11:56 +0530

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -3,12 +3,12 @@ const tasks =
 
     {
         "due":"March 13th",
-        "task":"CS Tutorial-1"
+        "task":"CS Tutorial-1(10)"
     },
 
     {
         "due":"March 13th onwards",
-        "task":"DS CS Review-1"
+        "task":"DS CS Review-1(10)"
     },
 
     {
@@ -23,46 +23,46 @@ const tasks =
 
     {
         "due":"April 16th",
-        "task":"DS Lab Eval-1"
+        "task":"DS Lab Eval-1(10)"
     },
 
     {
         "due":"April 16th",
-        "task":"POPL Lab Eval-2"
+        "task":"POPL Lab Eval-2(15)"
     },
 
     {
         "due":"April 17th",
-        "task":"CS Quiz-1"
+        "task":"CS Quiz-1(10)"
     },
 
     {
         "due":"April 19th",
-        "task":"SE Quiz-2"
+        "task":"SE Quiz-2(5)"
     },
 
     {
         "due":"April 24th",
-        "task":"DS Tutorial-2"
+        "task":"DS Tutorial-2(9)"
     },
 
     {
         "due":"May 7th",
-        "task":"DS Review-2"
+        "task":"DS Review-2(10)"
     },
 
     {
         "due":"May 13th",
-        "task":"POPL Quiz-2"
+        "task":"POPL Quiz-2(5)"
     },
 
     {
         "due":"May 14th",
-        "task":"POPL Lab Eval-3"
+        "task":"POPL Lab Eval-3(10)"
     },
 
     {
         "due":"May 15th",
-        "task":"CS Review"
+        "task":"CS Review(10)"
     },
 ]
