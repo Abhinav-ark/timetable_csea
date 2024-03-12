778e26312723836ea933a0db56cd40d9b30bbafc
Hariharan18 <93380054+Hariharan-Arul@users.noreply.github.com>
2024-03-13 00:00:56 +0530

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -1,24 +1,68 @@
 const tasks = 
 [
-    {
-        "due":"March 12th",
-        "task":"POPL Lab Eval-1"
-    },
 
     {
         "due":"March 13th",
+        "task":"CS Tutorial-1"
+    },
+
+    {
+        "due":"March 13th onwards",
         "task":"DS CS Review-1"
     },
 
     {
         "due":"March 15th",
-        "task":"CS Tutorial-1"
+        "task":"CIR Mock Interview (Batch-2)"
     },
-
-    
 
     {
         "due":"Starting April 1st",
         "task":"CIR Verbal Speech"
-    }
+    },
+
+    {
+        "due":"April 16th",
+        "task":"DS Lab Eval-1"
+    },
+
+    {
+        "due":"April 16th",
+        "task":"POPL Lab Eval-2"
+    },
+
+    {
+        "due":"April 17th",
+        "task":"CS Quiz-1"
+    },
+
+    {
+        "due":"April 19th",
+        "task":"SE Quiz-2"
+    },
+
+    {
+        "due":"April 24th",
+        "task":"DS Tutorial-2"
+    },
+
+    {
+        "due":"May 7th",
+        "task":"DS Review-2"
+    },
+
+    {
+        "due":"May 13th",
+        "task":"POPL Quiz-2"
+    },
+
+    {
+        "due":"May 14th",
+        "task":"POPL Lab Eval-3"
+    },
+
+    {
+        "due":"May 15th",
+        "task":"CS Review"
+    },
 ]
