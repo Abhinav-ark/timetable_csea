a7cddea70a576a29ed11a36f8792aa2460cf43dd
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-07-09 00:17:32 +0530

    Timetable sem 7 update

--- tasks.js
+++ tasks.js
@@ -1,61 +1,15 @@
 const tasks = 
 [
 
+    // Sample Task
+    // {
+    //     "due":"April 29th onwards",
+    //     "task":"CIR Verbal Speech"
+    // },
+    
     {
-        "due":"April 29th onwards",
-        "task":"CIR Verbal Speech"
+            "due":"-",
+            "task":"-"
     },
     
-
-    {
-        "due":"May 8th",
-        "task":"DS Lab Eval-1(10)"
-    },
-
-    {
-        "due":"May 9th",
-        "task":"DS Tutorial-2(9)"
-    },
-    
-
-    {
-        "due":"May 10th",
-        "task":"CS Review(10)"
-    },
-
-    {
-        "due":"May 10th - 16:30",
-        "task":"SE Quiz-3(20)"
-    },
-
-    {
-        "due":"May 10th - 11th",
-        "task":"DS Review-2(10)"
-    },
-
-
-    {
-        "due":"May 11th",
-        "task":"CS Quiz-1(10)"
-    },
-    
-
-    {
-        "due":"",
-        "task":"POPL Quiz-2(5)"
-    },
-
-
-    
-
-    
-    
-
-    
-
-    {
-        "due":"June 17th - 29th",
-        "task":"CIR Special classes"
-    }
-
 ]
