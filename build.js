6e692bca98acff9027e7a22faa3d1a07a7e0a3e2
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-07-10 02:03:44 +0530

    build bug fix

--- build.js
+++ build.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
-let htmlContent = fs.readFileSync('index.template.html', 'utf8');
+let htmlContent = fs.readFileSync('oauthCallback.template.html', 'utf8');
 htmlContent = htmlContent.replace('CLIENT_ID_PLACEHOLDER', clientId);
 htmlContent = htmlContent.replace('CLIENT_SECRET_PLACEHOLDER', clientSecret);
 
-fs.writeFileSync('index.html', htmlContent, 'utf8');
+fs.writeFileSync('oauthCallback.html', htmlContent, 'utf8');
 console.log('Build completed');
