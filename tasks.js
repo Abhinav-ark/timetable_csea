ddbc6c18fe5edc3941d2426cf03c7ef444043103
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-07-13 14:14:44 +0530

    edit Tasks snapshot (alpha)

--- tasks.js
+++ tasks.js
@@ -3,11 +3,13 @@ const tasks =
 
     // Sample Task
     // {
+    //     "subject":"CIR",
     //     "due":"April 29th onwards",
     //     "task":"CIR Verbal Speech"
     // },
     
     {
+            "subject":"-",
             "due":"-",
             "task":"-"
     },
