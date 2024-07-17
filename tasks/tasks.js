17b7586e1468ba2f3f5edee5244f7cfa1ab5a599
Abhinav Ramakrishnan <76396917+Abhinav-ark@users.noreply.github.com>
2024-07-18 02:41:13 +0530

    Update tasks.js

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -2,10 +2,9 @@ const tasks =
 [
 
     {
-		"subject": "CIR",
-		"due": "--",
-		"task": "--"
-	}
->>>>>>> 31a6640601e0f688392aa6a48c241d35f8a70adf
+	"subject": "CIR",
+	"due": "--",
+	"task": "--"
+    }
 ]
 
