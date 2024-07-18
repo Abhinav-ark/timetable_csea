53935e04dfe96933306cfc721c107083c8b8ff37
Hariharan18 <93380054+Hariharan-Arul@users.noreply.github.com>
2024-07-18 11:10:54 +0530

    Updated file from editor

--- tasks/tasksPE5.js
+++ tasks/tasksPE5.js
@@ -1,18 +1,14 @@
 const tasksPE5 = 
 [
-
-    // Sample Task
-    // {
-    //     "subject":"<subject>",
-    //     "due":"YYYY-MM-DD",
-    //     "task":"<task>"
-    // },
-    
-   
-  {
-         "subject":"DLCV",
-        "due":"2024-07-24",
-        "task":"Eval-1"
-  }
-    
+	{
+		"subject": "DLCV",
+		"due": "2024-07-24",
+		"task": "Eval-1"
+	},
+	{
+		"subject": "TSAF",
+		"due": "2024-08-01",
+		"task": "Lab Assignment - 1"
+	}
 ]
+
