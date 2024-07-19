2c0f05d3f6712e18b42b3b80cdda1f9a1d3c17f7
Abhinav Ramakrishnan <76396917+Abhinav-ark@users.noreply.github.com>
2024-07-19 14:10:07 +0530

    Updated file from editor

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -1,10 +1,14 @@
 const tasks = 
 [
-
-    {
-	"subject": "CIR",
-	"due": "--",
-	"task": "--"
-    }
+	{
+		"subject": "CIR",
+		"due": "--",
+		"task": "--"
+	},
+	{
+		"subject": "CD",
+		"due": "2024-07-26",
+		"task": "---"
+	}
 ]
 
