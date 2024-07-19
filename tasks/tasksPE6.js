5eb35d8dc497c97df4ca04400348d0669017dd01
Hariharan18 <93380054+Hariharan-Arul@users.noreply.github.com>
2024-07-19 22:29:20 +0530

    Updated file from editor

--- tasks/tasksPE6.js
+++ tasks/tasksPE6.js
@@ -4,6 +4,36 @@ const tasksPE6 =
 		"subject": "IR",
 		"due": "2024-08-13",
 		"task": "Tutorial-1"
+	},
+	{
+		"subject": "MAD",
+		"due": "2024-07-25",
+		"task": "Quiz - 1"
+	},
+	{
+		"subject": "MAD",
+		"due": "2024-08-06",
+		"task": "Lab Eval - 1"
+	},
+	{
+		"subject": "MAD",
+		"due": "2024-08-19",
+		"task": "Lab Eval - 2"
+	},
+	{
+		"subject": "MAD",
+		"due": "2024-09-20",
+		"task": "Quiz -2"
+	},
+	{
+		"subject": "MAD",
+		"due": "2024-10-03",
+		"task": "Lab Eval - 3"
+	},
+	{
+		"subject": "MAD",
+		"due": "2024-10-22",
+		"task": "Lab Eval - 4"
 	}
 ]
 
