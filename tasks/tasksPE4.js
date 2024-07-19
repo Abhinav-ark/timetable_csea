b709b250bb1e2271b63ed75e962c3d896b0165d3
Hariharan18 <93380054+Hariharan-Arul@users.noreply.github.com>
2024-07-19 22:22:09 +0530

    Updated file from editor

--- tasks/tasksPE4.js
+++ tasks/tasksPE4.js
@@ -1,3 +1,24 @@
 const tasksPE4 = 
-[]
+[
+	{
+		"subject": "RTS",
+		"due": "2024-07-31",
+		"task": "Lab Eval - 1"
+	},
+	{
+		"subject": "RTS",
+		"due": "2024-08-23",
+		"task": "Lab Eval - 2"
+	},
+	{
+		"subject": "RTS",
+		"due": "2024-10-10",
+		"task": "Quiz -1"
+	},
+	{
+		"subject": "RTS",
+		"due": "2024-10-24",
+		"task": "Quiz - 2"
+	}
+]
 
