25bda29ac1ee25d3ef562b0f6770a1b405087eea
Hariharan18 <93380054+Hariharan-Arul@users.noreply.github.com>
2024-07-20 10:52:49 +0530

    Updated file from editor

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -1,9 +1,29 @@
 const tasks = 
 [
 	{
-		"subject": "CIR",
-		"due": "--",
-		"task": "--"
+		"subject": "CD",
+		"due": "2024-08-27",
+		"task": "Lab Eval -1 "
+	},
+	{
+		"subject": "CD",
+		"due": "2024-08-30",
+		"task": "Quiz - 1"
+	},
+	{
+		"subject": "CD",
+		"due": "2024-10-09",
+		"task": "Quiz -2"
+	},
+	{
+		"subject": "CD",
+		"due": "2024-10-29",
+		"task": "Lab Eval - 2"
+	},
+	{
+		"subject": "CD",
+		"due": "2024-11-05",
+		"task": "Quiz -3"
 	}
 ]
 
