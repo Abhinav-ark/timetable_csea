6fdb1d49b683641b13c5454b2c8ac0a23d6bb066
Hariharan18 <93380054+Hariharan-Arul@users.noreply.github.com>
2024-07-25 23:20:10 +0530

    Updated file from editor

--- tasks/tasksPE4.js
+++ tasks/tasksPE4.js
@@ -60,43 +60,55 @@ const tasksPE4 =
 		"due": "2024-09-23",
 		"task": "Quiz - 2"
 	},
-    
-    {
+	{
 		"subject": "CGV",
 		"due": "2024-10-24",
 		"task": "Lab Eval - 2"
 	},
-  
-    {
+	{
 		"subject": "CGV",
 		"due": "2024-10-28",
 		"task": "CS Review Start"
 	},
-    
-    {
+	{
 		"subject": "IOT",
 		"due": "2024-07-25",
 		"task": "CS Sprint - 1"
 	},
-    {
+	{
 		"subject": "IOT",
 		"due": "2024-08-08",
 		"task": "CS Review - 2"
 	},
-    {
+	{
 		"subject": "IOT",
 		"due": "2024-08-26",
 		"task": "Tutorial - 1"
 	},
-    {
+	{
 		"subject": "IOT",
 		"due": "2024-09-19",
 		"task": "CS Sprint - 3"
 	},
-    {
+	{
 		"subject": "IOT",
 		"due": "2024-09-26",
 		"task": "CS Sprint - 2"
+	},
+	{
+		"subject": "EH",
+		"due": "2024-09-16",
+		"task": "CS Eval - 1"
+	},
+	{
+		"subject": "EH",
+		"due": "2024-10-10",
+		"task": "Tutorial"
+	},
+	{
+		"subject": "EH",
+		"due": "2024-10-14",
+		"task": "CS Eval - 2"
 	}
 ]
 
