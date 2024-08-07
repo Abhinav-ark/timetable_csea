4707be2713159b695ff6dcb2d7233585acbdcc51
Hariharan18 <93380054+Hariharan-Arul@users.noreply.github.com>
2024-08-07 13:46:38 +0530

    Updated file from editor

--- tasks/tasksPE5.js
+++ tasks/tasksPE5.js
@@ -32,7 +32,7 @@ const tasksPE5 =
 	},
 	{
 		"subject": "TSAF",
-		"due": "2024-08-24",
+		"due": "2024-08-21",
 		"task": "Quiz - 1"
 	},
 	{
@@ -60,41 +60,40 @@ const tasksPE5 =
 		"due": "2024-08-22",
 		"task": "Quiz - 1"
 	},
-    {
+	{
 		"subject": "DLCV",
 		"due": "2024-10-04",
 		"task": "Lab Eval - 2"
 	},
-    {
+	{
 		"subject": "DLCV",
 		"due": "2024-10-21",
 		"task": "Quiz - 2"
 	},
-    {
+	{
 		"subject": "DLCV",
 		"due": "2024-10-28",
 		"task": "CS Review Start"
 	},
-    {
+	{
 		"subject": "NNDL",
 		"due": "2024-08-14",
 		"task": "Lab Eval - 1"
 	},
-    {
+	{
 		"subject": "NNDL",
 		"due": "2024-09-11",
 		"task": "Tutorial - 2"
 	},
-    {
+	{
 		"subject": "NNDL",
 		"due": "2024-10-05",
 		"task": "Lab Eval - 2"
 	},
-    {
+	{
 		"subject": "NNDL",
 		"due": "2024-10-24",
 		"task": "Project Eval Start"
 	}
-  
 ]
 
